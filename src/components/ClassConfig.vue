129dcec3c73db8dc910d72215334826d462523cf
Paul Bouman <bouman@ese.eur.nl>
2021-02-03 19:04:43 +0100

    Added first components

---
+++ src/components/ClassConfig.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-container>
+      <v-row>
+        <v-col cols="3" v-for="(type, idx) of group_types" :key="type">
+          <v-text-field :label="type" v-model="group_count[idx]" type="number" min="0" />
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-card-actions>
+      <v-btn color="primary" @click="next">Volgende</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  const PREFIX = 'Groep' ;
+  const NUMBER_OF_YEARS = 8;
+  const GROUP_TYPES = [...Array(NUMBER_OF_YEARS).keys()].map(i => PREFIX + (i+1));
+
+  export default {
+    name: 'ClassConfig',
+    methods: {
+      next() {
+        this.$emit('next');
+      }
+    },
+    data: () => ({
+      group_types: [...GROUP_TYPES],
+      group_count: GROUP_TYPES.map(() => 1)
+    }),
+  }
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+    <v-stepper v-model="step" vertical>
+      <v-stepper-step :complete="step > 1" step="1">Aantal Klassen</v-stepper-step>
+      <v-stepper-content step="1">
+        <ClassConfig @next="step++" />
+      </v-stepper-content>
+      <v-stepper-step :complete="step > 2" step="2">Gemeenschappelijke Ouders</v-stepper-step>
+      <v-stepper-content step="2">
+        <v-card>Step 2</v-card>
+      </v-stepper-content>
+      <v-stepper-step :complete="step > 3" step="3">Berekenen Oplossing</v-stepper-step>
+      <v-stepper-content step="3">
+        <v-card>Step 3</v-card>
+      </v-stepper-content>
+      <v-stepper-step step="4">Bekijken Oplossing</v-stepper-step>
+      <v-stepper-content step="4">
+        <v-card>Step 4</v-card>
+      </v-stepper-content>
+    </v-stepper>
+  </v-container>
+</template>
+
+<script>
+  import ClassConfig from './ClassConfig';
+
+  export default {
+    name: 'Main',
+    components: {
+      ClassConfig
+    },
+    data: () => ({
+      step: 1
+    }),
+  }
+</script>
