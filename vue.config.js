90d12ebe10350c919924114ee0746ceaa7f4f293
Paul Bouman <bouman@ese.eur.nl>
2021-02-04 12:40:09 +0100

    Added configuration for Github Pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  outputDir: path.resolve(__dirname, "docs"),
+  publicPath: process.env.NODE_ENV === 'production' ? '/VeiligNaarSchool/' : '/',
+  pages: {
+    index: {
+      entry: './src/main.js',
+      title: 'Veilig Naar School'
+    }
+  }  
 }
